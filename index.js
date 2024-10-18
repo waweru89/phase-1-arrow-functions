93ab00841646c51c090771b177128d576e601eb6
MBURU <wawerumburu157@gmail.com>
2024-10-18 13:07:54 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide =(a, b)=>{
+return  a/b;
+};
+let a = 2000;
+let b = 100;
+console.log(divide(a,b));
+
+
+const square = (x) => {
+    return x * x; 
+};
+
+const add = (a, b) => {
+    return a + b;
+};
+
+
